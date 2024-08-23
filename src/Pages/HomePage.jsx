583ed144242c1a8980bef9ef62931f418ec8ee36
Alejandro Ochoa <115182554+ToastedPickle2@users.noreply.github.com>
2024-08-22 21:07:27 -0700

    Add files via upload

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Banner from "../components/Banner";
+import NavBar from "../components/NavBar";
+import styles from "./HomePage.module.css";
+
+export default function HomePage() {
+  return (
+    <>
+      <div>
+        <NavBar />
+        <Banner />
+      </div>
+    </>
+  );
+}
