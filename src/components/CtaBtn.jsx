3dc7ee87629644a1c2f0442d85067c6a34457a20
Alejandro Ochoa <115182554+ToastedPickle2@users.noreply.github.com>
2024-10-23 11:39:52 -0700

    Delete src directory

--- src/components/CtaBtn.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./CtaBtn.module.css";
-
-export default function LearnMoreBtn({ className, children }) {
-  return (
-    <button className={`${styles.btn} ${className || ""}`}>{children}</button>
-  );
-}
